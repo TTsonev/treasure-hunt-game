98a2ea5a104e57db35042c5e18b64d88f665df98
TTsonev <80343194+TTsonev@users.noreply.github.com>
2024-03-30 22:06:25 +0100

    Update GenericExampleException.java

--- code/server/src/main/java/server/exceptions/GenericExampleException.java
+++ code/server/src/main/java/server/exceptions/GenericExampleException.java
@@ -2,14 +2,6 @@ package server.exceptions;
 
 import org.apache.logging.log4j.util.Strings;
 
-/* Note, this is just a quick minimal example to show the generic exception handling
- * functionality in spring. Typically I would recommend that you create individual exception types
- * for each unique kind of exception (e.g., individual exceptions for different kinds of rule errors)
- * 
- * While doing so, follow the tips given in the Error Handling Tutorial.
- * 
- * For simplicity reasons, your own exceptions could, e.g., inherit from the one given below
- */
 public class GenericExampleException extends RuntimeException {
 
 	private final String errorName;
